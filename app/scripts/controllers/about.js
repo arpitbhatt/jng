1e70fa383f59a7305c33c2003ed4fa6dc815060f
arpitbhatt <arpit.bhatt@impetus.co.in>
2015-06-30 13:00:39 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkinsGruntApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jenkinsGruntApp
+ */
+angular.module('jenkinsGruntApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];    
+    
+  });
---
+++ bat.bat
@@ -0,0 +1,6 @@
+﻿set PATH=%PATH%;%appdata%\npm
+cd C:\Users\arpit.bhatt\Desktop\jenkins-grunt
+npm install &&^
+bower install --dev &&^
+grunt cibuild &&^
+echo "chal gaya"
